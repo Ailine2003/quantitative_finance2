a86c1cc6111ac66f341498d9ba07ac81aaf9b467
Ailine2003 <ailine.encina@gmail.com>
2025-12-02 23:01:58 +0000

    rama actualizada

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-from modules.financials_functions import portfolio_volatility
+import pandas as pd
+import numpy as np
+from modules.financial_functions import portfolio_volatility, portfolio_returns, VaR
+from modules.backend import tickers_by_issuer
+
+if _name_ =='_main_': 
+    #Obtener tickers de ishares
+    tickers = tickers_by_issuer(issuer= 'iShares')
+    list_tickers = list(tickers['TICKER'])
+    
+    for ticker in list_tickers:
+        print(f'Instrumento: {ticker}')
+    
+    #Portafolio de renta fija
+    tickers_rf = tickers[tickers['CATEGORIA']=='ETF RF']
+    list_tickers_rf = list(tickers_rf['TICKER'])
